0c71f8f9ad955653a916de31af9844b97923444a
Nazareno Carlesso <nazarenocarlessob@gmail.com>
2023-03-18 17:57:11 -0300

    first task done

---
+++ README.md
@@ -0,0 +1,12 @@
+# Prueba técnica para juniors y trainees de React
+
+#### APIs:
+
+- Facts Random: https://catfact.ninja/fact
+- Cat Random: https://cataas.com/cat
+
+#### Instrucciones:
+
+- Recupera un hecho aleatorio de gatos de la primera API
+- Recupera la primera palabra del hecho
+- Muestra una imagen de un gato con la primera palabra
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function App () {
+  const [fact, setFact] = useState()
+  const [words, setWords] = useState()
+  const [image, setImage] = useState()
+
+  // efecto que trae el hecho y sus primeras palabras
+  useEffect(() => {
+    fetch('https://catfact.ninja/fact')
+      .then(response => response.json())
+      .then(data => {
+        const { fact } = data
+        const firstWords = fact.split(' ', 3).join(' ')
+        // console.log(firstWords)
+        setFact(fact)
+        setWords(firstWords)
+      })
+  }, [])
+
+  // efecto que trae la imagen con las primeras palabras
+  useEffect(() => {
+    if (!fact) return
+
+    fetch(`https://cataas.com/cat/says/${words}?size=50&json=true`)
+      .then(response => response.json())
+      .then(data => {
+        const { url } = data
+        setImage(url)
+      })
+  }, [words])
+
   return (
-    <div>
+    <main style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
       <h1>App de gatitos</h1>
-    </div>
+      {fact && <p>{fact}</p>}
+      {image && <img src={`https://cataas.com/${image}`} alt={`Image extracted using the three first words for ${fact}`} />}
+    </main>
   )
 }
